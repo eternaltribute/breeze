376e95dd96b546ebbd2ed4775fe661de120880c1
Brittney Elizabeth Solarte <brittneyesolarte@Brittneys-MacBook-Pro.local>
2026-06-08 11:24:34 -0400

    feat(S1-016): build global app shell and navigation with sidebar layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react";
+//import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
@@ -8,22 +8,22 @@ import Settings from "./pages/Settings";
 function App() {
   return (
     <BrowserRouter>
-      <SignedOut>
+      {/* <SignedOut>
         <SignIn />
-      </SignedOut>
-      <SignedIn>
-        <div style={{ display: "flex" }}>
-          <Sidebar />
-          <div style={{ padding: "20px", flex: 1 }}>
-            <UserButton />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/settings" element={<Settings />} />
-            </Routes>
-          </div>
+      </SignedOut> */}
+      {/* <SignedIn> */}
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div style={{ padding: "20px", flex: 1 }}>
+          {/* <UserButton /> */}
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
         </div>
-      </SignedIn>
+      </div>
+      {/* </SignedIn> */}
     </BrowserRouter>
   );
 }
